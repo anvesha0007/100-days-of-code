26d6f806a05b3257c25608923f8b652a9aa25aca
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 20:45:46 +0530

    Add program to count even and odd numbers in array

---
+++ day30_1.c
@@ -0,0 +1,38 @@
+//Count even and odd numbers in an array.
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    // Read array elements
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Count even and odd numbers
+    for (i = 0; i < n; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+
+    printf("Number of even elements = %d\n", even);
+    printf("Number of odd elements = %d\n", odd);
+
+    return 0;
+}
+
