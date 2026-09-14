0924bfdc8776dcd31b35cd5513abb1191738ee86
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 17:18:39 +0530

    Add star printing program in day26_2.c
    This program prints groups of stars in different quantities: 4, 5, 3, and 1.

---
+++ day26_2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    // First group - 4 stars
+    for (j = 1; j <= 4; j++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+
+    // Second group - 5 stars
+    for (j = 1; j <= 5; j++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+
+    // Third group - 3 stars
+    for (j = 1; j <= 3; j++)
+    {
+        printf("*");
+    }
+    printf("\n\n");
+
+    // Fourth group - 1 star
+    printf("*\n");
+
+    return 0;
+}
+
