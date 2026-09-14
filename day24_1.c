adaa47cebbbb3865a656a4a75bd04cd109f82bda
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 17:00:21 +0530

    Add program to print asterisks in triangular pattern

---
+++ day24_1.c
@@ -0,0 +1,20 @@
+```c id="p4x8nm"
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+```
