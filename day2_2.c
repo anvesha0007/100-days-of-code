843846497e2908c78f85525a2425241e4477c1b0
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-15 00:46:52 +0530

    Clean up comments and HTML entities in day2_2.c
    Removed unnecessary comment formatting and fixed HTML entities.

--- day2_2.c
+++ day2_2.c
@@ -1,4 +1,4 @@
-```c
+//Write a program to calculate the area and circumference of a circle given its radius
 #include <stdio.h>
 
 int main()
@@ -17,4 +17,4 @@ int main()
 
     return 0;
 }
-```
+
