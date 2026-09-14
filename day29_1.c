b3e79cb46a46b1e5ed428818814d764138ee37cf
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 20:39:25 +0530

    Implement array sum calculation in C

---
+++ day29_1.c
@@ -0,0 +1,29 @@
+//Find the sum of array elements
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, sum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    // Read array elements
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // Calculate sum
+    for (i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+
+    printf("Sum of array elements = %d\n", sum);
+
+    return 0;
+}
+```
