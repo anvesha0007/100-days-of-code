20d7e6c651d659485009cb8936e597e671b7fc8f
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 16:50:39 +0530

    Add program to swap first and last digits of a number

---
+++ day21_1.c
@@ -0,0 +1,34 @@
+```c id="n8q3wt"
+#include <stdio.h>
+
+int main()
+{
+    int num, first, last, divisor, middle, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Find the last digit
+    last = num % 10;
+
+    // Find the divisor to get the first digit
+    divisor = 1;
+    while (num / divisor >= 10)
+    {
+        divisor = divisor * 10;
+    }
+
+    // Find the first digit
+    first = num / divisor;
+
+    // Find the middle part
+    middle = (num % divisor) / 10;
+
+    // Swap first and last digits
+    result = last * divisor + middle * 10 + first;
+
+    printf("Number after swapping first and last digit = %d", result);
+
+    return 0;
+}
+```
