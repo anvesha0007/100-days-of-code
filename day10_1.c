4b94904c1727e1a54433f9076e6e2c3375e752d1
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 12:52:16 +0530

    Add triangle type determination program
    This program determines the type of triangle based on the lengths of its sides.

---
+++ day10_1.c
@@ -0,0 +1,26 @@
+```c
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c;
+
+    printf("Enter the three sides of the triangle: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == b && b == c)
+    {
+        printf("The triangle is Equilateral.");
+    }
+    else if (a == b || b == c || a == c)
+    {
+        printf("The triangle is Isosceles.");
+    }
+    else
+    {
+        printf("The triangle is Scalene.");
+    }
+
+    return 0;
+}
+```
