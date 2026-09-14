19309117b3258d9aeda60c4b7c6dcd7e795b43d1
anvesha0007 <srivastava.anvesha1611@gmail.com>
2026-09-14 20:51:55 +0530

    Update day12_2.c

--- day12_2.c
+++ day12_2.c
@@ -1,4 +1,8 @@
-```c id="q1k5xm"
+//Write a program to calculate electricity bill based on units consumed with these rates: 
+//First 100 units at ₹5/unit 
+//Next 100 units at ₹7/unit 
+//Next 100 units at ₹10/unit 
+//Above at ₹12/unit
 #include <stdio.h>
 
 int main()
